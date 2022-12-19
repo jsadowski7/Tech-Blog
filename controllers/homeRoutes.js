7731db13afd183d7465555d068e23b655528a3b2
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-12-18 23:43:52 -0600

    Layed out bones for get routes in homeRoutes.js file

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Project, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/login', (req, res) => {
+
+});
+
+router.get('/signup', (req,res) => {
+
+});
+
+router.get('/post-comments', (req, res) => {
+
+});
+
+module.exports = router;
