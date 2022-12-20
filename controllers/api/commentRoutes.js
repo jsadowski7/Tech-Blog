16b9db2befbe04cb292e9dd05449dc875d167ec2
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-12-20 17:30:11 -0600

    Wrote get routes for commentRoutes.js file

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,12 +2,26 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  Comment.findAll({})
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  Comment.findAll({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', withAuth, (req, res) => {
