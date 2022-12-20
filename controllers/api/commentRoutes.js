6efeb70b25e9a518fb4a08665cc81ebd0841e6df
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-12-20 17:41:14 -0600

    Wrote put and post routes for commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { resolveConfig } = require('prettier');
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
@@ -24,16 +25,60 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post('/', withAuth, (req, res) => {
-
+router.post("/", withAuth, (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
-router.put('/', withAuth, (req, res) => {
-
+router.put("/", withAuth, (req, res) => {
+  Comment.update(
+    {
+      comment_text: req.body.comment_text,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment with this ID" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/', withAuth, (req, res) => {
-
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment with this ID'});
+            return;
+        }
+        res.json(dbCommentData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
