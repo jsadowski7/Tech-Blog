6c2d94f6f62f3c97b634fe7f02d622f2c93ec577
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-12-19 18:24:45 -0600

    Wrote single post get route in homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -44,6 +44,45 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+// Single Post Route
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post with this ID" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      // 
+      console.log(post);
+      // 
+      res.render("single-post", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // Post Comment Route
 router.get("/post-comments", (req, res) => {
   Post.findOne({
