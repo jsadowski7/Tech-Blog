8f3e3e5936b6a3a53d15464072fee0a58c98e9dd
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-12-22 23:24:18 -0600

    Fixed errors in handlebars, finished writing handlebars files. Edited error in post.js model

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,20 +1,23 @@
-const { Post } = require('../models');
+const { Post } = require("../models");
 
-const postData = [{
-    title: 'Lorem ipsum I',
-    content: 'Lorem ipsum dolor',
-    user_id: 1
-},
-{
-    title: 'Lorem ipsum II',
-    content: 'lorem ipsum dolor',
-    user_id: 2
-},
-{
-    title: 'Lorem III',
-    content: 'Lorem ipsum dolor',
-    user_id: 3
-}];
+const postData = [
+  {
+    title: "MVC",
+    content:
+      "MVC allows developers to maintain separation of concerns and devise code",
+    user_id: 1,
+  },
+  {
+    title: "ORMs",
+    content: "ORMs simplify creating queries in SQL!",
+    user_id: 2,
+  },
+  {
+    title: "Project",
+    content: "Going to use Axios for the first time!",
+    user_id: 3,
+  },
+];
 
 const seedPosts = () => Post.bulkCreate(postData);
 
